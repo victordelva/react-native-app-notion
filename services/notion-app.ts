906ba54dc71f51ae0448455b87dff3a367741cf4
Victor <vicalle49@gmail.com>
2023-04-01 21:13:20 +0200

    env

--- services/notion-app.ts
+++ services/notion-app.ts
@@ -1,5 +1,5 @@
-const apiBaseUrl = 'http://localhost:3001';
-
+import { API_URL } from '@env';
+const apiBaseUrl = API_URL;
 export interface PostData {
     title: string;
     image: {
@@ -8,7 +8,7 @@ export interface PostData {
 }
 
 export const getPosts = async (): Promise<PostData[]> => {
-
+    console.log(apiBaseUrl);
     try {
         const response = await fetch(`${apiBaseUrl}/api/pages`);
         return await response.json();
